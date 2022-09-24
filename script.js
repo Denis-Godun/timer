6fb2fb52e33c25e90a7a228efdb8cd51aac1ea55
Denis <hallo-boy@mail.ru>
2022-09-24 15:41:52 +0300

    timer work

--- script.js
+++ script.js
@@ -1,92 +1,98 @@
 //часы
-let hoursItem = document.querySelector('.hours');
-let minutesItem = document.querySelector('.minutes');
-let secondsItem = document.querySelector('.seconds');
-let milisecondsItem = document.querySelector('.miliseconds');
-
+let hoursItem = document.querySelector(".hours");
+let minutesItem = document.querySelector(".minutes");
+let secondsItem = document.querySelector(".seconds");
+let milisecondsItem = document.querySelector(".miliseconds");
 //кнопки
-const srartButton = document.querySelector('.start');
-const pauseButton = document.querySelector('.pause');
-const stopButton = document.querySelector('.stop');
-const newButton = document.querySelector('.new');
+const srartButton = document.querySelector(".start");
+const pauseButton = document.querySelector(".pause");
+const stopButton = document.querySelector(".stop");
+const newButton = document.querySelector(".new");
 
 //значения
 let hour = 00,
-minute = 00,
-second = 00,
-millisecond = 00,
-interval;
-
-//круги
-let fixed = document.querySelector('.results');
-
+  minute = 00,
+  second = 00,
+  millisecond = 00,
+  interval;
 
 //обработчики кнопок
 srartButton.onclick = () => {
-    clearInterval(interval)
-    interval = setInterval(startTimer,10)
+  clearInterval(interval);
+  interval = setInterval(startTimer, 10);
 };
 
 pauseButton.onclick = () => {
-    clearInterval(interval)
-}
-stopButton.onclick = () => {
-    clearInterval(interval) 
-    clearFields()
- 
-}
- 
-//функция для кнопки старт
-function startTimer(){
-    //секунды
-    millisecond++
-    if(millisecond < 9){
-        milisecondsItem.innerText = "0" + millisecond
-    }
-    if(millisecond > 9){
-        milisecondsItem.innerText = millisecond 
-    }
-    if(millisecond > 99){
-        second++
-        secondsItem.innerText = "0" + second
-        millisecond = 0
-        milisecondsItem.innerText = "0" + millisecond
-    }
-    //минуты
-    if(second < 9){
-        secondsItem.innerText = "0" + second 
-    }
-    if(second > 9){
-        secondsItem.innerText = second
-    }
-    if(second > 59){
-        minute++
-        minutesItem.innerText = "0" + minute
-        second = 0
-        secondsItem.innerText = "0" + second
-    }
-    //часы
-    if(minute < 9){
-        minutesItem.innerText = "0" + minute 
-    }
-    if(minute > 9){
-        minutesItem.innerText = minute
-    }
-    if(minute > 59){
-        hour++
-        hoursItem.innerText = "0" + hour
-        minute = 0
-        hoursItem.innerText = "0" + hour
-    }
+  clearInterval(interval);
 };
+stopButton.onclick = () => {
+  clearInterval(interval);
+  clearFields();
+  window.location.reload()
+};
+newButton.onclick = ()=> {
+    let result = document.querySelector('.result');
+    let li =  document.createElement('li');
+    li.className = "item";
+    li.innerText = `Результат ${hour}:${minute}:${second}:${millisecond}`
+    result.appendChild(li)
+}
+
+function createdLi(){
+  let li =  document.createElement('li');
+  li.className = "item";
+  li.textContent = hour.value + minute.value + second.value + millisecond.value;
+  document.querySelector('.result').appendChild(li);
+}
+
+//функция для кнопки старт
+function startTimer() {
+  //секунды
+  millisecond++;
+  if (millisecond < 9) {
+    milisecondsItem.innerText = "0" + millisecond;
+  }
+  if (millisecond > 9) {
+    milisecondsItem.innerText = millisecond;
+  }
+  if (millisecond > 99) {
+    second++;
+    secondsItem.innerText = "0" + second;
+    millisecond = 0;
+    milisecondsItem.innerText = "0" + millisecond;
+  }
+  //минуты
+  if (second < 9) {
+    secondsItem.innerText = "0" + second;
+  }
+  if (second > 9) {
+    secondsItem.innerText = second;
+  }
+  if (second > 59) {
+    minute++;
+    minutesItem.innerText = "0" + minute;
+    second = 0;
+    secondsItem.innerText = "0" + second;
+  }
+  //часы
+  if (minute < 9) {
+    minutesItem.innerText = "0" + minute;
+  }
+  if (minute > 9) {
+    minutesItem.innerText = minute;
+  }
+  if (minute > 59) {
+    hour++;
+    hoursItem.innerText = "0" + hour;
+    minute = 0;
+    hoursItem.innerText = "0" + hour;
+  }
+}
 
 function clearFields() {
-    hour = 00,
-    minute = 00,
-    second = 00,
-    millisecond = 00;
-    hoursItem.textContent = "00"
-    minutesItem.textContent = "00"
-    secondsItem.textContent = "00"
-    milisecondsItem.textContent = "00"
-};
+  (hour = 00), (minute = 00), (second = 00), (millisecond = 00);
+  hoursItem.textContent = "00";
+  minutesItem.textContent = "00";
+  secondsItem.textContent = "00";
+  milisecondsItem.textContent = "00";
+}
